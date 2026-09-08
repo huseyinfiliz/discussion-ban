57f3774931c7f42d005daa63cdeb163575f77e55
Hüseyin Filiz <mysuperuser01@gmail.com>
2026-09-08 16:26:30 +0300

    fix: register discussion-bans model in app store

--- js/src/common/index.ts
+++ js/src/common/index.ts
@@ -1,2 +1,8 @@
-export { default as extend } from './extend';
+import app from 'flarum/common/app';
+import DiscussionBan from './models/DiscussionBan';
+
+app.initializers.add('huseyinfiliz-discussion-ban-common', () => {
+  app.store.models['discussion-bans'] = DiscussionBan;
+});
+
 export { default as DiscussionBan } from './models/DiscussionBan';
